d0bfd740678f1363796f46317e75d4e977e28625
Pat Haynes <pathaynes32@gmail.com>
2019-10-30 13:37:49 -0700

    added characters and css

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,15 @@ import Header from './header/Header';
 import Footer from './footer/Footer';
 import Character from './character/Character';
 import rick from '../data/rick.png';
+import beth from '../data/beth.jpg';
+import jerry from '../data/jerry.jpg';
+import mecese from '../data/mecese.jpg';
+import morty from '../data/morty.jpg';
+import goomonster from '../data/goomonster.jpg';
+import bernie from '../data/bernie.jpg';
+import pickles from '../data/pickles.jpg';
+import summer from '../data/summer.jpg';
+import mrPoopyButthole from '../data/mr_poopy_butthole.jpg';
 
 const App = () => (
   <>
@@ -11,6 +20,46 @@ const App = () => (
       name="Rick"
       species="Human"
       img={rick} />
+    <Character
+      name="Beth"
+      species="Human"
+      img={beth} />
+    <Character
+      name="jerry"
+      species="Human"
+      img={jerry} />
+    <Character
+      name="mecese"
+      species="mecese"
+      img={mecese} />
+    <Character
+      name="morty"
+      species="morty"
+      img={morty} />
+    <Character
+      name="goomonster"
+      species="goomonster"
+      img={goomonster} />
+    <Character
+      name="bernie"
+      species="bernie"
+      img={bernie} />
+    <Character
+      name="pickles"
+      species="pickles"
+      img={pickles} />
+    <Character
+      name="pickles"
+      species="goomonster"
+      img={goomonster} />
+    <Character
+      name="summer"
+      species="summer"
+      img={summer} />
+    <Character
+      name="Mr Poopy Butthole"
+      species="alien"
+      img={mrPoopyButthole} />
     <Footer />
   </>
 );
--- src/components/character/Character.js
+++ src/components/character/Character.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Character.css';
 
-function Character({ name, species, img }) {
-  return (
-    <dl className={styles.Character}>
-      <dl>{name}</dl>
-      <dl>{species}</dl>
-      <img src={img} />
-    </dl>
-  );
+export default class Character extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    species: PropTypes.string.isRequired,
+    img: PropTypes.string
+  };
+
+  render() {
+    const { name, species, img } = this.props;
+    return (
+      <div className={styles.CharacterContainer}>
+        <dl className={styles.Character}>
+          <dl>{name}</dl>
+          <dl>{species}</dl>
+          <img src={img} />
+        </dl>
+      </div>
+    );
+  }
 }
 
-Character.propTypes = {
-  name: PropTypes.string.isRequired,
-  species: PropTypes.string.isRequired,
-  img: PropTypes.string
-};
 
-export default Character;
+
